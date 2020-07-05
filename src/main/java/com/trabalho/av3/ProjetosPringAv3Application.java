3131e03cab3177aca712e7d6623587cfe9c2709e
frazaoalex <alexfrazaodev@gmail.com>
2020-07-05 15:23:22 -0300

    Iniciar projeto no STS

---
+++ src/main/java/com/trabalho/av3/ProjetosPringAv3Application.java
@@ -0,0 +1,12 @@
+package com.trabalho.av3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ProjetosPringAv3Application{
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetosPringAv3Application.class, args);
+	}
+}
---
+++ src/test/java/com/trabalho/av3/ProjetosPringAv3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trabalho.av3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetosPringAv3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
